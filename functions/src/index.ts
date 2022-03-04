20ba6f58b0487a13747f6659914089878bd4fc16
DaiKodama <daikodama@icloud.com>
2022-03-04 23:19:22 +0900

    firestore追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,9 @@
-import * as functions from 'firebase-functions';
-import express from 'express';
+import * as functions from "firebase-functions";
 
-const app = express();
-
-app.get('/api', (req, res) => {
-  res.send('hello firebase.');
-});
-
-export const fn = functions.https.onRequest(app);
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
